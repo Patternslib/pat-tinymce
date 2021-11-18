0f5874c2c2b702568a9c8471c521f1512465e9f9
Johannes Raggam <thetetet@gmail.com>
2021-11-18 01:27:27 +0100

    maint(build): Add webpack module federation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
 process.traceDeprecation = true;
 const path = require("path");
 const patternslib_config = require("@patternslib/patternslib/webpack/webpack.config.js");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = async (env, argv) => {
     let config = {
         entry: {
-            bundle: path.resolve(__dirname, "bundle-config.js"),
+            bundle: path.resolve(__dirname, "index.js"),
         },
     };
 
     config = patternslib_config(env, argv, config);
     config.output.path = path.resolve(__dirname, "dist/");
+
+    config.plugins.push(
+        new ModuleFederationPlugin({
+            shared: {
+                "@patternslib/patternslib": {
+                    singleton: true,
+                },
+                "jquery": {
+                    requiredVersion: dependencies.jquery,
+                    singleton: true,
+                },
+                "underscore": {
+                    version: dependencies.underscore,
+                    requiredVersion: dependencies.underscore,
+                    singleton: true,
+                },
+            },
+        })
+    );
 
     return config;
 };
